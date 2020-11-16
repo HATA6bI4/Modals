01327dd17ac0252235546ac9d6720a8ad983f087
HATA6bI4 <59885730+HATA6bI4@users.noreply.github.com>
2020-11-16 22:32:19 +0600

    1

---
+++ src/components/ModalValidate.vue
@@ -0,0 +1,28 @@
+<template>
+ <modal 
+    title="Modal with form + Validate"
+    @close="$emit('close')">
+    
+      <div slot="body">
+         <form @submit.prevent="">
+           <label>Name:</label>
+           <input type="text" required>
+           <label>Email:</label>
+           <input type="email" required>
+          <button class="btn btnPrimary">Submit</button>
+         </form>
+       </div>
+  </modal>
+  
+</template>
+
+<script>
+import modal from '@/components/UI/Modal.vue'
+export default {
+    components: { modal }
+}
+</script>
+
+<style>
+
+</style>template>
